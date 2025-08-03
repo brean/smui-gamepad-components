0651a7f2a52dd4ea5c398ce23f927492b1f335d6
Andreas Bresser <self@andreasbresser.de>
2025-08-03 15:45:22 +0200

    export Tabbar and Slider

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
 // Reexport your entry components here
 import Button from "./components/Button.svelte";
 import List from "./components/List.svelte";
+import TabBar from "./components/TabBar.svelte";
+import Slider from "./components/Slider.svelte";
 
-export { Button, List };
+export { Button, List, Slider, TabBar };
